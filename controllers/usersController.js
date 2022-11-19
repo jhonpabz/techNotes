185f9c925bc1c32e72be0dab000632d169371bc1
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-19 14:57:54 +0800

    Update userController: Added logic to updateUser.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -51,7 +51,46 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @desc Update a users
 // @routes PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, username, roles, active, password } = req.body;
+
+  // Confirm data
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== 'boolean'
+  ) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+
+  // Check duplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
+  // Allow updates to the original user
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: 'Duplicate username' });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    // Hash password
+    user.password = await bcrypt.hash(password, 10); // salt rounds
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({ message: `${updatedUser.username} updated` });
+});
 
 // @desc Delete a users
 // @routes DELETE /users
