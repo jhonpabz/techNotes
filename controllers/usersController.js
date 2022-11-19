7555e866c144db918f7fdfb448f0d43f1edbbca1
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-19 22:16:09 +0800

    Update user controller.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -102,8 +102,8 @@ const deleteUser = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: 'User ID Required' });
   }
 
-  const notes = await Note.findOne({ user: id }).lean().exec();
-  if (notes?.length) {
+  const note = await Note.findOne({ user: id }).lean().exec();
+  if (note) {
     return res.status(400).json({ message: 'User has assigned notes' });
   }
 
