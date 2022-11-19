9792b0da61e75555fbf74c1eaa8335741d5c22d5
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-19 18:06:55 +0800

    Update userController: Added deleteUser.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -95,7 +95,29 @@ const updateUser = asyncHandler(async (req, res) => {
 // @desc Delete a users
 // @routes DELETE /users
 // @access Private
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ message: 'User ID Required' });
+  }
+
+  const notes = await Note.findOne({ user: id }).lean().exec();
+  if (notes?.length) {
+    return res.status(400).json({ message: 'User has assigned notes' });
+  }
+
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+
+  const result = await user.deleteOne();
+
+  const reply = `Username ${result.username} with ID ${result._id} has been deleted`;
+  res.json(reply);
+});
 
 module.exports = {
   getAllUsers,
