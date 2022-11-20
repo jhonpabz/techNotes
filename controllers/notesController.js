9d530eb1a50676cb8b8c1dfaeb43cd0a120e6f05
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-20 10:22:35 +0800

    Add username to each note before sending the response

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -10,7 +10,16 @@ const getAllNotes = asyncHandler(async (req, res) => {
   if (!notes?.length) {
     return res.status(400).json({ message: 'No notes found' });
   }
-  res.json(notes);
+
+  // Add username to each note before sending the response
+  const notesWithUser = await Promise.all(
+    notes.map(async (note) => {
+      const user = await User.findById(note.user).lean().exec();
+      return { ...note, username: user.username };
+    })
+  );
+
+  res.json(notesWithUser);
 });
 
 // @desc Create new notes
@@ -24,7 +33,7 @@ const createNewNote = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: 'All fields are required' });
   }
 
-  const userCreatedNotes = await User.username;
+  const userCreatedNotes = await User.findOne({ id }).lean().exec();
   const noteObject = { user: userCreatedNotes, title, text, completed };
   // Create and store new user
   const note = await Note.create(noteObject);
