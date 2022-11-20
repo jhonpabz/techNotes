8c6de0a9c94afcf323dafc37d56b127fe0727543
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-20 21:10:11 +0800

    Update url that can access api.

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 // Input here inside an array a sites that u allow to access your REST API
 const allowedOrigins = [
   'http://localhost:3000',
-]
+  'http://127.0.0.1:5173/',
+  'http://127.0.0.1:5173',
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
