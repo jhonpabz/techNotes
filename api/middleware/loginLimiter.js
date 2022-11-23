aec095740f0d5ed05eaafda70ae3a4732db9323a
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-23 23:51:34 +0800

    Fix error. Added export module.

--- api/middleware/loginLimiter.js
+++ api/middleware/loginLimiter.js
@@ -18,3 +18,5 @@ const loginLimiter = rateLimit({
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
+
+module.exports = loginLimiter;
