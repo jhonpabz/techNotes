5a7ba9e3cb54510ae97a53674fc78d38f1f81272
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-23 20:16:43 +0800

    Added logout.

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -90,7 +90,13 @@ const refresh = (req, res) => {
 // @desc Logout
 // @route POST /auth/logout
 // @access Public - just to clear cookie if exist
-const logout = (req, res) => {};
+const logout = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); // No content
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+
+  res.json({ message: 'Cookie cleared' });
+};
 
 module.exports = {
   login,
