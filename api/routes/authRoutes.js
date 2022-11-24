7c4ada5fb010c7eda08146a596ec72b7f9ae4bb7
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-11-24 08:34:03 +0800

    Fix bug on auth refresh and logout endpoint.

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -5,8 +5,8 @@ const loginLimiter = require('../middleware/loginLimiter');
 
 router.route('/').post(loginLimiter, authController.login);
 
-router.route('./refresh').get(authController.refresh);
+router.route('/refresh').get(authController.refresh);
 
-router.route('./logout').post(authController.logout);
+router.route('/logout').post(authController.logout);
 
 module.exports = router;
