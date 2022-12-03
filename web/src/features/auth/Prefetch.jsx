ece9ebc2c18690ade62d241860754f4e3827c73b
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-12-03 22:35:55 +0800

    Update prefetch.

--- web/src/features/auth/Prefetch.jsx
+++ web/src/features/auth/Prefetch.jsx
@@ -6,15 +6,12 @@ import { Outlet } from "react-router-dom";
 
 const Prefetch = () => {
   useEffect(() => {
-    console.log("subscribing");
-    const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate());
-    const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
-
-    return () => {
-      console.log("unsubscribing");
-      notes.unsubscribe();
-      users.unsubscribe();
-    };
+    store.dispatch(
+      notesApiSlice.util.prefetch("getNotes", "noteList", { force: true })
+    );
+    store.dispatch(
+      notesApiSlice.util.prefetch("getUsers", "userList", { force: true })
+    );
   }, []);
 
   return <Outlet />;
